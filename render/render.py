a65ee6b4d1dbaceb61c02e2306ea3cb7fe413012
Justin Lewis <justinlewis@apple.com>
2023-06-24 18:56:45 -0700

    update

--- render/render.py
+++ render/render.py
@@ -11,8 +11,6 @@ from PIL import Image
 import logging
 import math
 
-MAX_LINES = 4
-
 class RenderHelper:
 
     def __init__(self, width, height, angle):
@@ -86,7 +84,7 @@ class RenderHelper:
         # weatherDict = {'high': 75, "low": 55, "pop": 10, "id": 801}
         # first setup list to represent the 5 weeks in our calendar
         calList = []
-        for i in range(35):
+        for i in range(28):
             calList.append([])
 
         # retrieve calendar configuration
@@ -158,7 +156,7 @@ class RenderHelper:
             event_count = len(calList[i])
             for j in range(min(event_count, maxEventsPerDay)):
                 event = calList[i][j]
-                event_line_limit = max(math.floor(MAX_LINES / event_count), 1)
+                event_line_limit = max(math.floor(maxEventsPerDay + 1 / event_count), 1)
                 cal_events_text += '<div {0}'.format('class="event-white' if red 
                     else 'style="font-size:1rem;max-height:{0}rem;padding:0.1rem;{1}overflow:hidden;text-overflow:ellipsis;border-radius:0rem;margin-bottom:1px;'.format(
                         1.5 * event_line_limit, "color: #6c757d!important;" if currDate.month != calDict['today'].month and not red else "color:black;"))
