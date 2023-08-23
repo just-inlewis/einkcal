cd55ae48b03e4ec29519fbf7be6ff5d215daa904
just-inlewis <98793314+just-inlewis@users.noreply.github.com>
2023-08-23 09:19:34 -0700

    Update power.py

--- power/power.py
+++ power/power.py
@@ -11,7 +11,7 @@ import logging
 class PowerHelper:
 
     def __init__(self):
-        self.logger = logging.getLogger('maginkcal')
+        self.logger = logging.getLogger('einkcal')
 
     def get_battery(self):
         # start displaying on eink display
