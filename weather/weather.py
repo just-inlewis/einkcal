2020200b259f0bcaccd6de6de3623d05f6e9651d
just-inlewis <98793314+just-inlewis@users.noreply.github.com>
2023-08-23 09:17:15 -0700

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -12,12 +12,22 @@ from datetime import datetime
 
 class WeatherHelper:
     def __init__(self):
-        self.logger = logging.getLogger('maginkdash')
+        self.logger = logging.getLogger('einkcal')
+
+    def retry_strategy(self):
+        return requests.adapters.Retry(
+            total=3,
+            status_forcelist=[500, 502, 503, 504],
+            backoff_factor=2,
+            method_whitelist=False
+        )
 
     def get_weather(self, lat, lon, api_key):
         url = "https://api.openweathermap.org/data/3.0/onecall?lat={0}&lon={1}&appid={2}&exclude=current,minutely,hourly,alerts&units=metric".format(
         lat, lon, api_key)
-        response = requests.get(url)
+        session = requests.Session()
+        session.mount("https://", requests.adapters.HTTPAdapter(max_retries=self.retry_strategy()))
+        response = session.get(url)
         data = json.loads(response.text)
         for forcast in data.get('daily'):
             if datetime.utcfromtimestamp(forcast.get('dt')).date() == datetime.today().date():
